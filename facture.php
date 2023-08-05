63e39c1accf7d066f73605f559112cfd61dff249
vick95 <vicarosel@gmail.com>
2023-08-05 09:03:25 +0200

    create new projet

---
+++ facture.php
@@ -0,0 +1,112 @@
+<?php 
+
+
+
+
+
+
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap.min.css">
+ 	<link rel="stylesheet" type="text/css" href="assets/css/facturestyle.css">
+ 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+ 	<title>MYFACT_PANNEL</title>
+ </head>
+ <body>
+ 	<div class="header">
+ 		<div class="container">
+ 			<div class="row">
+ 				<div class="col-xs-3 col-md-3">
+ 					<img src="assets/image/logo.jpg" class="logo">
+ 				</div>
+ 				<div class="col-xs-6 col-md-6 twohearder">
+ 					<h2 class="def">TOUTES PRESTATION EN CONCEPTION DE PANNEAUX LUMINAIRES</h2>
+ 					<h2 class="def">COMMUNICATION-PUBLICITE SUPPORT-BTP</h2>
+ 					<h2 class="def">GESTION IMMOBILIERE-DIVERSES PRESTATIONS DE SERVICES</h2>
+ 				</div>
+ 				<div class="col-xs-3 col-md-3 threehearder">
+ 					<h3 class="identr">RCCM RB ABC/18-A-8700</h3>
+ 					<h3 class="identr">IFU:1201702257202</h3>
+ 				</div>
+ 			</div>
+ 			<h6 class="adr">Sainte Rita à 50m de la mairie de Cotonou au bord des pavés du marché de Wologuèdè en face du portail de l'ecole maternelle de Sainte Rita</h6>
+ 			<div class="sidebarre"><hr></div>
+ 		</div>
+ 	</div>
+ 	<div class="headfact">
+ 		<div class="container">
+ 			<div class="row">
+ 				<div class=" col-xs-12 col-md-12 right">
+ 					<h4>FACTURE DE VENTE</h4>
+ 					<h5 class="nfac">Facture N°: </h5>
+ 					<h6 class="facdate">Date:</h6>
+ 				</div>
+ 				<div class=" col-xs-12 col-md-12 left">
+ 					<button type="submit" class="form-control btn btn-primary btclt">Gerer le client</button>
+ 					<div class="card">
+ 						<label>NOM :</label>
+ 						<label>PRENOM :</label>
+ 						<label>TEL :</label>
+ 					</div>
+ 				</div>
+ 			</div>	
+ 		</div>
+ 	</div>
+ 	<div class="bodyfac">
+ 		<div class="container">
+ 			<div class="row">
+ 				<div class="col-xs-12 col-md-12">
+ 					<button type="submit" class="form-control btn btn-primary btfact">Editer la facture</button>
+ 					<table class="table table-striped table-bordered">
+ 						<thead>
+ 						<tr>
+ 							<th>QTE</th>
+ 							<th>DESIGNATION ET REFERENCE <br> DU BIEN</th>
+ 							<th>PU <br>(FCFA)</th>
+ 							<th>REMISE <br> (FCFA)</th>
+ 							<th>MONTANT <br> (FCFA)</th>		
+ 						</thead>
+ 						<tbody>
+ 							<tr>
+ 								<td>qte</td>
+ 								<td>
+ 									<table>
+ 										<tr>
+		 									<td>de</td>
+ 										</tr>
+ 										<tr>
+ 											<td>ac</td>
+ 										</tr>
+ 									</table>
+ 								</td>
+ 								<td>pu</td>
+ 								<td>re</td>
+ 								<td>mon</td>		
+ 							</tr>
+ 						</tbody>
+ 					</table>
+ 					<table class="partysum">
+ 						<tr>
+		 					<td>remise</td>
+		 					<td>montant achat</td>
+ 						</tr>
+ 					</table>
+ 					
+ 				</div>
+ 				
+ 			</div>
+ 			
+ 		</div>
+ 		
+ 	</div>
+
+ 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+ </body>
+ </html>
